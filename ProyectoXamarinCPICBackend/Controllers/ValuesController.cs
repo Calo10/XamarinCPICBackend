b9635de1106265dbb9d72a06375d65e51b8d3808
Carlos Mendez <carlos_mendez_10@live.com>
2018-12-09 12:12:19 -0600

    First Upload

---
+++ ProyectoXamarinCPICBackend/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoXamarinCPICBackend.Models;
+
+namespace ProyectoXamarinCPICBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        public static List<CustomerModel> lstAllCustomers = new List<CustomerModel>();
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<CustomerModel>> Get()
+        {
+
+            return lstAllCustomers;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<bool> Post([FromBody] CustomerModel customer)
+        {
+            lstAllCustomers.Add(customer);
+
+            return true;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProyectoXamarinCPICBackend/Models/CustomerModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ProyectoXamarinCPICBackend.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Detail
+        {
+            get;
+            set;
+        }
+
+        public string Gender
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public CustomerModel()
+        {
+        }
+    }
+}
